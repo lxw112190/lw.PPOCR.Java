787e8cb5bdbff31514aec06f976ce35fe24e9998
lxw112190 <819069052@qq.com>
2026-09-12 19:00:27 +0800

    block vector pointwise convolution outputs

--- lw-ppocr-vector/src/main/java/io/github/lxw112190/ppocr/vector/VectorBackend.java
+++ lw-ppocr-vector/src/main/java/io/github/lxw112190/ppocr/vector/VectorBackend.java
@@ -376,6 +376,96 @@ public final class VectorBackend implements KernelBackend {
         for (int n = 0; n < batch; n++) {
             for (int group = 0; group < groups; group++) {
                 int oc = 0;
+                for (; oc + 7 < outputsPerGroup; oc += 8) {
+                    int channel0 = group * outputsPerGroup + oc;
+                    int output0 = outputOffset + (n * outputChannels + channel0) * plane;
+                    int output1 = output0 + plane;
+                    int output2 = output1 + plane;
+                    int output3 = output2 + plane;
+                    int output4 = output3 + plane;
+                    int output5 = output4 + plane;
+                    int output6 = output5 + plane;
+                    int output7 = output6 + plane;
+                    int weight0 = weightOffset + channel0 * inputsPerGroup;
+                    int weight1 = weight0 + inputsPerGroup;
+                    int weight2 = weight1 + inputsPerGroup;
+                    int weight3 = weight2 + inputsPerGroup;
+                    int weight4 = weight3 + inputsPerGroup;
+                    int weight5 = weight4 + inputsPerGroup;
+                    int weight6 = weight5 + inputsPerGroup;
+                    int weight7 = weight6 + inputsPerGroup;
+                    float bias0 = bias == null ? 0.0f : bias[biasOffset + channel0];
+                    float bias1 = bias == null ? 0.0f : bias[biasOffset + channel0 + 1];
+                    float bias2 = bias == null ? 0.0f : bias[biasOffset + channel0 + 2];
+                    float bias3 = bias == null ? 0.0f : bias[biasOffset + channel0 + 3];
+                    float bias4 = bias == null ? 0.0f : bias[biasOffset + channel0 + 4];
+                    float bias5 = bias == null ? 0.0f : bias[biasOffset + channel0 + 5];
+                    float bias6 = bias == null ? 0.0f : bias[biasOffset + channel0 + 6];
+                    float bias7 = bias == null ? 0.0f : bias[biasOffset + channel0 + 7];
+                    int i = 0;
+                    for (; i < bound; i += SPECIES.length()) {
+                        FloatVector result0 = FloatVector.broadcast(SPECIES, bias0);
+                        FloatVector result1 = FloatVector.broadcast(SPECIES, bias1);
+                        FloatVector result2 = FloatVector.broadcast(SPECIES, bias2);
+                        FloatVector result3 = FloatVector.broadcast(SPECIES, bias3);
+                        FloatVector result4 = FloatVector.broadcast(SPECIES, bias4);
+                        FloatVector result5 = FloatVector.broadcast(SPECIES, bias5);
+                        FloatVector result6 = FloatVector.broadcast(SPECIES, bias6);
+                        FloatVector result7 = FloatVector.broadcast(SPECIES, bias7);
+                        for (int ic = 0; ic < inputsPerGroup; ic++) {
+                            int inputBase = inputOffset +
+                                    (n * channels + group * inputsPerGroup + ic) * plane;
+                            FloatVector sample = FloatVector.fromArray(SPECIES, input, inputBase + i);
+                            result0 = result0.add(sample.mul(weights[weight0 + ic]));
+                            result1 = result1.add(sample.mul(weights[weight1 + ic]));
+                            result2 = result2.add(sample.mul(weights[weight2 + ic]));
+                            result3 = result3.add(sample.mul(weights[weight3 + ic]));
+                            result4 = result4.add(sample.mul(weights[weight4 + ic]));
+                            result5 = result5.add(sample.mul(weights[weight5 + ic]));
+                            result6 = result6.add(sample.mul(weights[weight6 + ic]));
+                            result7 = result7.add(sample.mul(weights[weight7 + ic]));
+                        }
+                        result0.intoArray(output, output0 + i);
+                        result1.intoArray(output, output1 + i);
+                        result2.intoArray(output, output2 + i);
+                        result3.intoArray(output, output3 + i);
+                        result4.intoArray(output, output4 + i);
+                        result5.intoArray(output, output5 + i);
+                        result6.intoArray(output, output6 + i);
+                        result7.intoArray(output, output7 + i);
+                    }
+                    for (; i < plane; i++) {
+                        float result0 = bias0;
+                        float result1 = bias1;
+                        float result2 = bias2;
+                        float result3 = bias3;
+                        float result4 = bias4;
+                        float result5 = bias5;
+                        float result6 = bias6;
+                        float result7 = bias7;
+                        for (int ic = 0; ic < inputsPerGroup; ic++) {
+                            int inputBase = inputOffset +
+                                    (n * channels + group * inputsPerGroup + ic) * plane;
+                            float sample = input[inputBase + i];
+                            result0 += sample * weights[weight0 + ic];
+                            result1 += sample * weights[weight1 + ic];
+                            result2 += sample * weights[weight2 + ic];
+                            result3 += sample * weights[weight3 + ic];
+                            result4 += sample * weights[weight4 + ic];
+                            result5 += sample * weights[weight5 + ic];
+                            result6 += sample * weights[weight6 + ic];
+                            result7 += sample * weights[weight7 + ic];
+                        }
+                        output[output0 + i] = result0;
+                        output[output1 + i] = result1;
+                        output[output2 + i] = result2;
+                        output[output3 + i] = result3;
+                        output[output4 + i] = result4;
+                        output[output5 + i] = result5;
+                        output[output6 + i] = result6;
+                        output[output7 + i] = result7;
+                    }
+                }
                 for (; oc + 3 < outputsPerGroup; oc += 4) {
                     int channel0 = group * outputsPerGroup + oc;
                     int output0 = outputOffset + (n * outputChannels + channel0) * plane;
